805177f95392e450192533966013b7ca1aa14bec
Andy Schunke <schunke.andy@gmail.com>
2022-12-18 20:57:28 +0100

    [refactor]store data locally, delete DB fetch for data

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./database/db.js";
-import { initialLoadData } from "./lib/initialLoadData.js";
-import routes from "./router/routes.js";
+import router from "./router/routes.js";
 import cors from "cors";
 
 dotenv.config();
@@ -10,10 +9,9 @@ connectDB();
 
 const server = express();
 const port = process.env.PORT || 5555;
-server.use(cors({ origin: "*" }));
-server.use("/", routes);
+server.use(cors());
+server.use("/", router);
 
 server.listen(port, async (req, res) => {
   console.log(`Server running under ${port}`);
-  initialLoadData();
 });
