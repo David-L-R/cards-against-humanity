9737d85428f26825641e22b71349007917b8e7fc
Danni Malka <dannimalka.iag@gmail.com>
2022-12-23 12:42:50 +0200

    few corrections as requested

--- client/cards-against-humaniy-client/pages/index.js
+++ client/cards-against-humaniy-client/pages/index.js
@@ -1,34 +1,12 @@
-import io from "socket.io-client";
-import { useEffect, useState } from "react";
+import React from "react";
+import Features from "./features/index";
 
-const socket = io.connect("http://localhost:5555");
-
-export default function Home() {
-  const [message, setMessage] = useState("");
-  const [messageReceived, setMessageReceived] = useState("");
-
-  const sendMessage = () => {
-    socket.emit("send_message", { message });
-  };
-
-  useEffect(() => {
-    socket.on("receive_message", (data) => {
-      setMessageReceived(data.message);
-    });
-  }, [socket]);
-
+function Home() {
   return (
-    <div>
-      <h1>Socket.io</h1>
-      <input
-        placeholder="Message..."
-        onChange={(event) => {
-          setMessage(event.target.value);
-        }}
-      />
-      <button onClick={sendMessage}>Send Message</button>
-      <h2>Message</h2>
-      <p>{messageReceived}</p>
-    </div>
+    <>
+      <Features />
+    </>
   );
 }
+
+export default Home;
