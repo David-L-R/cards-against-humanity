85ab088921ae411f26a951ef989b936eeb5f5d8e
Danni Malka <dannimalka.iag@gmail.com>
2022-12-23 15:51:14 +0200

    pulled main

--- server/index.js
+++ server/index.js
@@ -34,21 +34,3 @@ const port = process.env.PORT || 5555;
 server.listen(port, async (req, res) => {
   console.log(`Server running under ${port}`);
 });
-
-//import express from "express";
-//import dotenv from "dotenv";
-//import connectDB from "./database/db.js";
-//import router from "./router/routes.js";
-//import cors from "cors";
-//
-//dotenv.config();
-//connectDB();
-//
-//const server = express();
-//const port = process.env.PORT || 5555;
-//server.use(cors());
-//server.use("/", router);
-//
-//server.listen(port, async (req, res) => {
-//  console.log(`Server running under ${port}`);
-//});
