a384059e4d2cc87e9c6b324517d90d1ed150604b
Danni Malka <dannimalka.iag@gmail.com>
2023-01-11 15:12:15 +0200

    wip stylings to czar page

--- client/components/Czar.jsx
+++ client/components/Czar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import style from "../styles/cardTemplate.module.css";
-import { motion as m, transform } from "framer-motion";
+import { AnimatePresence, motion as m } from "framer-motion";
 
 const Czar = ({
   blackCards,
@@ -26,10 +26,10 @@ const Czar = ({
     return setshowBlackCards((prev) => [...cardsToDisplay]);
   };
 
-  const selectCard = ({ index }) => {
+  const selectCard = ({ index, element }) => {
     //update all black cards
     /*
-    
+    if (index) {
       element.classList.add("testClass");
       setActiveIndex(index);
     }
@@ -42,6 +42,8 @@ const Czar = ({
       setCardsOnTable((prev) => {
         const newDeck = { ...prev };
         newDeck.table.cards = [selected.card];
+        element.classList.add("testClass");
+        setActiveIndex(index);
         return newDeck;
       });
       setshowBlackCards(null);
@@ -70,42 +72,59 @@ const Czar = ({
     randomBlackCards();
   }, []);
   console.log("gameStage", gameStage);
+
   if (!showBlackCards || gameStage !== "black") return;
 
   return (
-    <section className="czarSelectionContainer">
-      <div>
-        <h1 className="czarPickingH1">You are the Czar!</h1>
-        <h2>Select a Card </h2>
-      </div>
-      <div>
-        <ul className="czarPickingContainer">
-          {showBlackCards &&
-            showBlackCards.map((cardItem, blackIndex) => (
-              <li
-                key={cardItem.card.text}
-                data-index={cardItem.index}
-                className={
-                  blackIndex === (showBlackCards.length - 1) / 2
-                    ? "middle"
-                    : blackIndex === showBlackCards.length - 1
-                    ? "highest"
-                    : "lowest"
-                }
-              >
-                {cardItem.index === activeIndex ? (
-                  <m.div
-                    initial={
-                      blackIndex === (showBlackCards.length - 1) / 2
-                        ? {
-                            top: "30%",
-                            left: "43%",
-                          }
-                        : blackIndex === showBlackCards.length - 1
-                        ? { top: "30%", left: "58%" }
-                        : { top: "30%", left: "28%" }
-                    }
-                    animate={{
+    <AnimatePresence>
+      <section className="czarSelectionContainer">
+        <div>
+          <h1 className="czarPickingH1">You are the Czar!</h1>
+          <h2>Select a Card </h2>
+        </div>
+        <div>
+          <ul className="czarPickingContainer">
+            {showBlackCards &&
+              showBlackCards.map((cardItem, blackIndex) => (
+                <li
+                  key={cardItem.card.text}
+                  data-index={cardItem.index}
+                  className={
+                    blackIndex === (showBlackCards.length - 1) / 2
+                      ? "middle"
+                      : blackIndex === showBlackCards.length - 1
+                      ? "highest"
+                      : "lowest"
+                  }
+                >
+                  {cardItem.index === activeIndex ? (
+                    <m.div
+                      key={cardItem.card.text}
+                      initial={
+                        blackIndex === (showBlackCards.length - 1) / 2
+                          ? {
+                              top: "30%",
+                              left: "43%",
+                            }
+                          : blackIndex === showBlackCards.length - 1
+                          ? { top: "30%", left: "58%" }
+                          : { top: "30%", left: "28%" }
+                      }
+                      animate={{
+                        top: "50%",
+                        left: "50%",
+                        translateX: "-50%",
+                        translateY: "-50%",
+                        zIndex: "500000",
+                        opacity: 1,
+                        scale: 2,
+                        rotate: 360,
+                        position: "fixed",
+                      }}
+                      transition={{ duration: 0.3 }}
+                      exit={{ opacity: 0, transition: { duration: 1 } }}
+                      /*
+                    exit={{
                       top: "50%",
                       left: "50%",
                       translateX: "-50%",
@@ -115,36 +134,38 @@ const Czar = ({
                       scale: 2,
                       rotate: 360,
                       position: "fixed",
-                    }}
-                    transition={{ duration: 0.3 }}
-                    exit={{
-                      x: -1300,
-                      opacity: 1,
-                      rotate: -120,
                       transition: { duration: 0.75 },
                     }}
-                    className={` ${style.black} czarPicking`}
-                    onClick={(e) => {
-                      selectCard({ index: cardItem.index, element: e.target });
-                    }}
-                  >
-                    {cardItem.card.text}
-                  </m.div>
-                ) : (
-                  <div
-                    className={` ${style.black} czarPicking`}
-                    onClick={(e) => {
-                      selectCard({ index: cardItem.index, element: e.target });
-                    }}
-                  >
-                    {cardItem.card.text}
-                  </div>
-                )}
-              </li>
-            ))}
-        </ul>
-      </div>
-    </section>
+                    */
+                      className={` ${style.black} czarPicking`}
+                      onClick={(e) => {
+                        selectCard({
+                          index: cardItem.index,
+                          element: e.target,
+                        });
+                      }}
+                    >
+                      {cardItem.card.text}
+                    </m.div>
+                  ) : (
+                    <div
+                      className={` ${style.black} czarPicking`}
+                      onClick={(e) => {
+                        selectCard({
+                          index: cardItem.index,
+                          element: e.target,
+                        });
+                      }}
+                    >
+                      {cardItem.card.text}
+                    </div>
+                  )}
+                </li>
+              ))}
+          </ul>
+        </div>
+      </section>
+    </AnimatePresence>
   );
 };
 
