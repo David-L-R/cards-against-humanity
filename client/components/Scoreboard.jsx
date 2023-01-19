71a2677684fe0dbafaa5faae38c38edc97208a0f
Danni Malka <dannimalka.iag@gmail.com>
2023-01-19 15:10:37 +0200

    scoreboard additions

--- client/components/Scoreboard.jsx
+++ client/components/Scoreboard.jsx
@@ -36,11 +36,18 @@ const Scoreboard = ({ currentLobby }) => {
   return (
     <>
       <ul>
+        <div className="fuckingClass">
+          <h4>
+            turn: {turns.length}/{currentLobby.setRounds}
+          </h4>
+          <h4>Players: {players.length}</h4>
+        </div>
         {players &&
           players.map((player) => (
             <li
               key={player.id}
-              className={player.inactive ? "inactive-player" : null}>
+              className={player.inactive ? "inactive-player" : null}
+            >
               <div>
                 {czar && czar.id === player.id && (
                   <div className={"crown-background"}>
