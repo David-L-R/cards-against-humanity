4fda69271cc194d0211c51280489a69067a34fd9
Danni Malka <dannimalka.iag@gmail.com>
2023-02-02 09:48:45 +0200

    added clo message

--- client/components/layout.js
+++ client/components/layout.js
@@ -5,6 +5,14 @@ import Head from "next/Head";
 const Layout = ({ children, socket, ...props }) => {
   return (
     <>
+      {console.log(
+        "%cWant to know how we did it? %cAsk us %cAndy Schunke LinkedIn: https://www.linkedin.com/in/andy-schunke/ %cDanni Malka LinkedIn: https://www.linkedin.com/in/danni-malka-58a5b1117/",
+        "padding: 50px 20% 10px 20%; color: white; font-family: Arial; font-size: 4em; font-weight: bolder; text-shadow: #000 5px 5px; background: black;",
+        "padding: 10px 45% 10px 50%; color: white; font-family: Arial; font-size: 1.5em; font-weight: normal; text-shadow: #000 1px 1px; background: black; width: 100%;",
+        "background: black; padding: 10px 30% 10px 30%;",
+        "background: black; padding: 10px 30% 50px 30%;"
+      )}
+
       <Head>
         <link
           rel="apple-touch-icon"
